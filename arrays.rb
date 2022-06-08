b6785c2d2bf025e8435698179551d60705b693a7
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-06-08 07:16:26 -0600

    Use all 4 methods required on all 4 arrays

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,16 @@ friends = ["Leah", "Allie", "Eddie", "Whitney", "Sydney", "Alaina"]
 ages = [43, 12, 16, 72, 12, 15, 16]
 salaries = [325.13, 125.12, 129472178.12, 4368.45]
 awesomenesses = [true, true, true, true]
+
+friends.pop
+# The pop method permanently changes the array it is called on.
+# It breaks off the final element in the array and returns it.
+ages.shift
+# The pop method permanently changes the array it is called on.
+# It breaks off the first element in the array and returns it.
+salaries.push("696.69")
+# The pop method permanently changes the array it is called on.
+# It adds on the argument to the end of the array and returns the entire array.
+awesomenesses.unshift(true)
+# The pop method permanently changes the array it is called on.
+# It adds on the argument to the beginning of the array and returns the entire array.
