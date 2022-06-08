61115bfbd4e4b726c8555ec7e10cffddaf2b1f57
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-06-08 07:07:40 -0600

    Add four variables

---
+++ arrays.rb
@@ -0,0 +1,4 @@
+friends = ["Leah", "Allie", "Eddie", "Whitney", "Sydney", "Alaina"]
+ages = [43, 12, 16, 72, 12, 15, 16]
+salaries = [325.13, 125.12, 129472178.12, 4368.45]
+awesomenesses = [true, true, true, true]
